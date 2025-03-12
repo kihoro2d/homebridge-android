ff0c71a53ab7c49d1ac22c61ba0a56172e8cb956
Ihor Kladko <kihoro2d@gmail.com>
2025-03-12 05:39:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,11 @@ echo '== Setting up Dpkg options ==' &&
   echo '== Updating repositories and upgrading packages ==' &&
   pkg update -y &&
   pkg upgr -y &&
-  echo '== Installing python, openssl and nodejs ==' &&
-  pkg i -y python openssl-1.1 nodejs-lts &&
+  echo '== Installing python, openssl, nodejs and proot ==' &&
+  pkg i -y python openssl nodejs-lts proot &&
+  echo '== Creating a stub /proc/stat file ==' &&
+  echo 'cpu 1132 34 1441 11311718 3675 127 438' > ~/stat &&
+  echo 'cpu0 1132 34 1441 11311718 3675 127 438' >> ~/stat &&
   echo '== Removing added Dpkg options ==' &&
   rm ~/../usr/etc/apt/apt.conf.d/local &&
   echo '== Installing homebridge and homebridge-config-ui ==' &&
@@ -17,8 +20,11 @@ echo '== Setting up Dpkg options ==' &&
   npm install -g --unsafe-perm homebridge-config-ui-x &&
   echo '== Creating default config ==' &&
   mkdir -p ~/.homebridge &&
-  echo -e '{\n	"bridge": {\n		"name": "Homebridge BA3D",\n		"username": "0E:F1:D3:85:BA:3D",\n		"port": 51248,\n		"pin": "171-94-744",\n		"advertiser": "bonjour-hap"\n	},\n	"accessories": [],\n	"platforms": [\n		{\n			"name": "Config",\n			"port": 8581,\n			"platform": "config",\n			"log": {\n				"method": "file",\n				"path": "/data/data/com.termux/files/home/.homebridge/homebridge.log"\n			}\n		}\n	]\n}\n' > ~/.homebridge/config.json &&
+  echo -e '{\n        "bridge": {\n                "name": "Homebridge BA3D",\n                "username": "0E:F1:D3:85:BA:3D",\n                "port": 51248,\n                "pin": "171-94-744",\n                "advertiser": "bonjour-hap"\n        },\n        "accessories": [],\n        "platforms": [\n                {\n                        "name": "Config",\n                        "port": 8581,\n                        "platform": "config",\n                        "log": {\n                                "method": "file",\n                                "path": "/data/data/com.termux/files/home/.homebridge/homebridge.log"\n                        }\n                }\n        ]\n}\n' > ~/.homebridge/config.json &&
   echo '== Adding homebridge commands ==' &&
   echo 'exec npx homebridge "$@" 2>&1 | tee ~/.homebridge/homebridge.log' > ~/../usr/bin/hb &&
   chmod +x ~/../usr/bin/hb &&
-  echo -e '== Installation successful ==\nExecute hb command to start'
+  echo 'alias hb-start="proot -b ~/stat:/proc/stat hb"' >> ~/.profile &&
+  chmod +x ~/.profile &&
+  . ~/.profile &&
+  echo -e '== Installation successful ==\nRestart Termux via exit command. Then execute hb-start command to launch Homebridge'
